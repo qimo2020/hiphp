23fa6a72747f0f7479a1c42b5102a8e6b0071d5d
祈陌 <1718360859@qq.com>
2020-07-18 21:18:46 +0800

    v1.0.0

--- version.php
+++ version.php
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 <?php
 return [
     'hiphp' => [
   'name' => 'HiPHP',
-  'version' => '1.0.2',
+  'version' => '1.0.0',
   'copyright' => 'HiPHP.NET',
   'url' => 'http://www.hiphp.net',
 ]
 ];
-=======
-<?php
-return [
-  'hiphp' =>
-  [
-     'name' => 'HiPHP',
-     'version' => '1.0.0',
-     'copyright' => 'HiPHP.NET',
-     'url' => 'http://www.hiphp.net'
-  ],
-];
->>>>>>> d52ad045fd7ed93be13a3b7bc1a6f4fbc770c8c7
