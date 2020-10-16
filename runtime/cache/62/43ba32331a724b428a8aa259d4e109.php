166934166e4147639a6794750fa1874b0594d3f6
qimo <1718360859@qq.com>
2020-10-16 10:55:46 +0800

    core6.0.4

--- runtime/cache/62/43ba32331a724b428a8aa259d4e109.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:3:{i:0;a:2:{s:4:"hook";s:10:"cloud_push";s:7:"plugins";s:5:"cloud";}i:1;a:2:{s:4:"hook";s:10:"cloud_temp";s:7:"plugins";s:5:"cloud";}i:2;a:2:{s:4:"hook";s:14:"system_builder";s:7:"plugins";s:7:"builder";}}
--- runtime/cache/62/ec31be4b860fa23220f7911fd016f4.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:2:{i:0;s:87:"D:\panchuming\phpstudy\WWW\hiphp_up\runtime\cache\bd\016d97e0cc568883c6d29f3d8d586f.php";i:1;s:87:"D:\panchuming\phpstudy\WWW\hiphp_up\runtime\cache\38\929dcf9309d592ce01e51243fe16e1.php";}
